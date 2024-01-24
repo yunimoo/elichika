69a5ee4db9ae1ff77ea6ef58fa18fa35d3c04e3b
Arina Aria <arina999999997@gmail.com>
2024-01-24 17:17:34 +0700

    Update install script

--- bin/install.sh
+++ bin/install.sh
@@ -1,10 +1,13 @@
-# install this version of elichika from scratch
-# download this file manually and run it
-# assume this is a fresh install
+# install the latest version of elichika from scratch
+# set the environmental variable BRANCH to pick a specific branch (latest version only)
+# this can be set with export or just set when invoking bash
+# Honestly this is only for testing but you can think of it as a hidden feature
+# if BRANCH is not provided, then default to master
+BRANCH=${BRANCH:-"master"}
 # install git and golang
 pkg install golang git -y || echo "assuming go and git are already installed"
 # clone the source code
-git clone https://github.com/arina999999997/elichika.git && \
+git clone --depth 1 https://github.com/arina999999997/elichika.git --branch $BRANCH --single-branch && \
 cd elichika && \
 # get the submodules (i.e. assets and other)
 git submodule update --init && \
@@ -13,4 +16,4 @@ go build && \
 # set the permission
 chmod +rx elichika && \
 echo "Installed succesfully!"
-# todo: edit .bashrc to make an easier command
+# TODO: maybe edit .bashrc to make an easier command
